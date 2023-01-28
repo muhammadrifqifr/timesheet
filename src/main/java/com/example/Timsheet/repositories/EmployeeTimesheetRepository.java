3331ba57c49168c7a36f4b15bf6378f84b1e1b63
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-29 01:33:40 +0700

    updated

--- src/main/java/com/example/Timsheet/repositories/EmployeeTimesheetRepository.java
+++ src/main/java/com/example/Timsheet/repositories/EmployeeTimesheetRepository.java
@@ -1,10 +1,20 @@
 package com.example.Timsheet.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.example.Timsheet.DTO.EmployeeTimesheetDTO;
 import com.example.Timsheet.models.EmployeeTimesheet;
+
 
 @Repository
 public interface EmployeeTimesheetRepository extends JpaRepository<EmployeeTimesheet, Integer> {
+    @Query(value = "SELECT * FROM db_timesheet.tbl_tr_employee_timesheet;", nativeQuery = true)
+    public List<EmployeeTimesheet> getEmployeeTimesheet();
     
+    @Query(value = "SELECT * FROM db_timesheet.tbl_tr_employee_timesheet;", nativeQuery = true)
+    public List<EmployeeTimesheetDTO> getEmployeeTimesheetDTO();
 }
