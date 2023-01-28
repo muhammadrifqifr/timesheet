2dc3ee26a5edd5ec48e24ffd68ca1e33dd0ce9f8
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-28 22:46:53 +0700

    updated

--- src/main/java/com/example/Timsheet/repositories/DepartmentRepository.java
+++ src/main/java/com/example/Timsheet/repositories/DepartmentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.example.Timsheet.DTO.DepartmentDTO;
 import com.example.Timsheet.models.Department;
 
 @Repository
@@ -14,14 +15,11 @@ public interface DepartmentRepository extends JpaRepository<Department, Integer>
 //      @Query("select new com.example.Timsheet.models.Department(u.id, u.name, u.division_id) from tbl_m_department u")
 //    public List<Department> getAllDepartment();
 
- @Query(nativeQuery = true,
-            value = "SELECT * tbl_m_department.division_id as departmenId, tbl_m_division.name, tbl_m_department.name as department, tbl_m_department.id "
-            + "FROM tbl_m_department "
-            + "INNER JOIN tbl_m_division "
-            + "ON tbl_m_department.division_id=tbl_m_division.id")
+    @Query(value = "SELECT * FROM tbl_m_department", nativeQuery = true)
     public List<Department> getAllDepartment();
 
-    //  @Query(value = "  JOIN  ON  =", nativeQuery = true)
-    // List<Department> getAllDepartment();
+    @Query(value = "SELECT * FROM tbl_m_department", nativeQuery = true)
+    public List<DepartmentDTO> getAllDepartmentDTO();
 
+ 
 }
