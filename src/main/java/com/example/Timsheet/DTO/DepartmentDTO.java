055a03e27569d44f76728f1f63ad215ccf5eda69
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-28 22:46:28 +0700

    updated

---
+++ src/main/java/com/example/Timsheet/DTO/DepartmentDTO.java
@@ -0,0 +1,31 @@
+package com.example.Timsheet.DTO;
+
+public class DepartmentDTO {
+    private Integer id;
+    private String name;
+
+    public DepartmentDTO(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DepartmentDTO(){}
+
+    
+}
