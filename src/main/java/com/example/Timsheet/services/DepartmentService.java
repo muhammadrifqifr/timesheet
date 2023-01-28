e7520008290e62aedfd505174ec1efd71c92483a
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-28 22:47:45 +0700

    updated

--- src/main/java/com/example/Timsheet/services/DepartmentService.java
+++ src/main/java/com/example/Timsheet/services/DepartmentService.java
@@ -1,10 +1,14 @@
 package com.example.Timsheet.services;
 
 import java.util.List;
+
+import com.example.Timsheet.DTO.DepartmentDTO;
 import com.example.Timsheet.models.Department;
 
 public interface DepartmentService {
     public List<Department> getAll();
+    public List<Department> getAllDepartment();
+    public List<DepartmentDTO> getAllDepartmentDTO();
     public Department getById(Integer id);
     public boolean save(Department department);
     public boolean delete(Integer id);
--- src/main/java/com/example/Timsheet/services/DepartmentServiceImpl.java
+++ src/main/java/com/example/Timsheet/services/DepartmentServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.Timsheet.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.Timsheet.DTO.DepartmentDTO;
 import com.example.Timsheet.models.Department;
 import com.example.Timsheet.repositories.DepartmentRepository;
 
@@ -14,7 +18,7 @@ public class DepartmentServiceImpl implements DepartmentService{
 
     @Override
     public List<Department> getAll() {
-        return departmentRepository.getAllDepartment();
+        return departmentRepository.findAll();
     }
 
     @Override
@@ -32,6 +36,22 @@ public class DepartmentServiceImpl implements DepartmentService{
     public boolean delete(Integer id) {
         departmentRepository.deleteById(id);
         return !departmentRepository.findById(id).isPresent();
+    }
+
+    @Override
+    public List<DepartmentDTO> getAllDepartmentDTO() {
+        List<Department> department = departmentRepository.getAllDepartment();
+        return department.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    public DepartmentDTO convertToDto(Department department){
+        return new DepartmentDTO(department.getId(),
+            department.getName());
+    }
+
+    @Override
+    public List<Department> getAllDepartment() {
+        return departmentRepository.getAllDepartment();
     }
     
 }
