3236ad39ae0d34024f690f58894224f5cb6bffc5
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-29 14:23:28 +0700

    updated

--- src/main/java/com/example/Timsheet/repositories/StatusRepository.java
+++ src/main/java/com/example/Timsheet/repositories/StatusRepository.java
@@ -1,10 +1,19 @@
 package com.example.Timsheet.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.example.Timsheet.DTO.StatusDTO;
 import com.example.Timsheet.models.Status;
 
 @Repository
 public interface StatusRepository extends JpaRepository<Status, Integer>{
-    
+    @Query(value = "SELECT * FROM tbl_tr_status", nativeQuery = true)
+    public List<Status> getAllStatus();
+
+    @Query(value = "SELECT * FROM tbl_tr_status", nativeQuery = true)
+    public List<StatusDTO> getAllStatusDTO();
 }
