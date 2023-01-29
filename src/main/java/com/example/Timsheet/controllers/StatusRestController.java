4d0ed45b3eea0880aa0daa324b54794f369a78bc
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-29 14:22:01 +0700

    updated

--- src/main/java/com/example/Timsheet/controllers/StatusRestController.java
+++ src/main/java/com/example/Timsheet/controllers/StatusRestController.java
@@ -1,6 +1,9 @@
 package com.example.Timsheet.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,13 +11,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.Timsheet.DTO.StatusDTO;
+import com.example.Timsheet.models.Status;
 import com.example.Timsheet.services.StatusService;
 
 @RestController
 @RequestMapping("/api/status")
 public class StatusRestController {
-    @Autowired
     private StatusService statusService;
+
+    @Autowired
+    public StatusRestController(StatusService statusService){
+        this.statusService = statusService;
+    }
+
+    @GetMapping()
+    public List<Status> getAll(){
+        return statusService.getAll();
+    }
+
+    @GetMapping("/statusDTO")
+    public List<StatusDTO> getAllDTO(){
+        return statusService.getAllStatusDTO();
+    }
 
     @PostMapping("/insert")
     public Boolean submitStatus(@RequestBody StatusDTO dto){
