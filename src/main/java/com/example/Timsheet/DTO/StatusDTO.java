231c0472784e31b426f663f90a55f8672f0364eb
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-29 14:22:27 +0700

    Add files via upload

--- src/main/java/com/example/Timsheet/DTO/StatusDTO.java
+++ src/main/java/com/example/Timsheet/DTO/StatusDTO.java
@@ -3,33 +3,50 @@ package com.example.Timsheet.DTO;
 import java.util.Date;
 
 public class StatusDTO {
-    private String Name;
-    private Date Date;
-    private Integer Employee;
-    private Integer Timesheet;
+    public StatusDTO(Integer id, String name, Date date, Integer employee_id, Integer timesheet_id) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.employee_id = employee_id;
+        this.timesheet_id = timesheet_id;
+    }
+        
+    private Integer id;
+    private String name;
+    private Date date;
+    private Integer employee_id;
+    private Integer timesheet_id;
 
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
     public String getName() {
-        return Name;
+        return name;
     }
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
     public Date getDate() {
-        return Date;
+        return date;
     }
     public void setDate(Date date) {
-        Date = date;
+        this.date = date;
     }
-    public Integer getEmployee() {
-        return Employee;
+    public Integer getEmployee_id() {
+        return employee_id;
     }
-    public void setEmployee(Integer employee) {
-        Employee = employee;
+    public void setEmployee_id(Integer employee_id) {
+        this.employee_id = employee_id;
     }
-    public Integer getTimesheet() {
-        return Timesheet;
+    public Integer getTimesheet_id() {
+        return timesheet_id;
     }
-    public void setTimesheet(Integer timesheet) {
-        Timesheet = timesheet;
+    public void setTimesheet_id(Integer timesheet_id) {
+        this.timesheet_id = timesheet_id;
     }
+
+   
 }
