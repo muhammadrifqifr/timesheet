6ed3e003af7c3c0129c1d8d84399a4dbcf4e3263
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-30 16:43:21 +0700

    updated

--- src/main/java/com/example/Timsheet/repositories/StatusRepository.java
+++ src/main/java/com/example/Timsheet/repositories/StatusRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.example.Timsheet.DTO.RequestToHrDTO;
+import com.example.Timsheet.DTO.RequestToPmDTO;
+import com.example.Timsheet.DTO.RequestToPmoDTO;
 import com.example.Timsheet.DTO.StatusDTO;
 import com.example.Timsheet.models.Status;
 
@@ -16,4 +19,22 @@ public interface StatusRepository extends JpaRepository<Status, Integer>{
 
     @Query(value = "SELECT * FROM tbl_tr_status", nativeQuery = true)
     public List<StatusDTO> getAllStatusDTO();
+
+    @Query(value = "SELECT s.*, ts.*, e.* FROM tbl_tr_status s JOIN tbl_m_employee e ON s.employee_id = e.id JOIN tbl_m_timesheet ts ON s.timesheet_id = s.id WHERE s.name  = 'request'", nativeQuery = true)
+    public List<Status> getStatusByProjectManager();
+
+    @Query(value = "SELECT s.*, ts.*, e.* FROM tbl_tr_status s JOIN tbl_m_employee e ON s.employee_id = e.id JOIN tbl_m_timesheet ts ON s.timesheet_id = s.id WHERE s.name  = 'request'", nativeQuery = true)
+    public List<RequestToPmDTO> getStatusByProjectManagerDTO();
+
+    // @Query(value = "SELECT s.*, ts.*, e.* FROM tbl_tr_status s JOIN tbl_m_employee e ON s.employee_id = e.id JOIN tbl_m_timesheet ts ON s.timesheet_id = s.id WHERE s.name  = request", nativeQuery = true)
+    // public List<Status> getStatusByProjectManagerOfficer();
+
+    // @Query(value = "SELECT s.*, ts.*, e.* FROM tbl_tr_status s JOIN tbl_m_employee e ON s.employee_id = e.id JOIN tbl_m_timesheet ts ON s.timesheet_id = s.id WHERE s.name  = request", nativeQuery = true)
+    // public List<RequestToPmoDTO> getStatusByProjectManagerOfficerDTO();
+
+    // @Query(value = "SELECT s.*, ts.*, e.* FROM tbl_tr_status s JOIN tbl_m_employee e ON s.employee_id = e.id JOIN tbl_m_timesheet ts ON s.timesheet_id = s.id WHERE s.name  = request", nativeQuery = true)
+    // public List<Status> getStatusByHumanResource();
+
+    // @Query(value = "SELECT s.*, ts.*, e.* FROM tbl_tr_status s JOIN tbl_m_employee e ON s.employee_id = e.id JOIN tbl_m_timesheet ts ON s.timesheet_id = s.id WHERE s.name  = request", nativeQuery = true)
+    // public List<RequestToHrDTO> getStatusByHumanResourceDTO();
 }
